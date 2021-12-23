8cc678d38d89be11f8ea465acf21092c807eca77
sfilyk <84185411+sfilyk@users.noreply.github.com>
2021-12-23 07:51:38 +0200

    Update BasePage.cs

--- PageObject/BasePage.cs
+++ PageObject/BasePage.cs
@@ -51,19 +51,19 @@ namespace PageObject
         }
     }
 
-    //public static class WebDriverExtensions
-    //{
-    //    public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
-    //    {
-    //        if (timeoutInSeconds > 0)
-    //        {
-    //            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
-    //            return wait.Until(drv => drv.FindElement(by));
-    //        }
-    //        return driver.FindElement(by);
-    //    }
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                return wait.Until(drv => drv.FindElement(by));
+            }
+            return driver.FindElement(by);
+        }
 
-    //}
+    }
     public static class MyAssert
     {
         public static bool MyIsTrue(string first, string second)
