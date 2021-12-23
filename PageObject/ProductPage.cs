934a2c40f3efbf7e7ee69b194085a7d77d2eb8a5
sfilyk <84185411+sfilyk@users.noreply.github.com>
2021-12-23 07:50:32 +0200

    Update ProductPage.cs

--- PageObject/ProductPage.cs
+++ PageObject/ProductPage.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 
 namespace PageObject
 {
@@ -6,13 +6,13 @@ namespace PageObject
     {
         public ProductPage(IWebDriver _driver) : base(_driver) { }
 
-        private readonly By _buttonChoiceByPrice = By.CssSelector("select[_ngcontent-rz-client-c42='']");
-        private readonly By _buttonChoiceByPriceExpensive = By.CssSelector("select[_ngcontent-rz-client-c42=''] option[value='2: expensive']");
+        private readonly By _buttonChoiceByPrice = By.XPath("//*[contains(@class,'catalog-settings__sorting')] //*[contains(@class,'')]");
+        private readonly By _buttonChoiceByPriceExpensive = By.CssSelector("option[value='2: expensive']");
         private readonly By _buttonIconAddProductBay = By.XPath("//*[@class ='toOrder ng-star-inserted']");
         private readonly By _buttonProductsInCart = By.XPath("//button[@class ='header__button ng-star-inserted header__button--active']");
 
 
-        public void buttonChoiceByPric() => WaitAndFindElement(_buttonChoiceByPrice).Click();
+        public void buttonChoiceByPric() => _driver.FindElement(_buttonChoiceByPrice,3000).Click();
         public void buttonChoiceByPriceExpensive() => WaitAndFindElement(_buttonChoiceByPriceExpensive).Click();
         public void buttonIconAddProductBay() => WaitAndFindElement(_buttonIconAddProductBay).Click();
         public void waitButtonIconAddProductBay() => Waitforelement(_driver, _buttonIconAddProductBay);
